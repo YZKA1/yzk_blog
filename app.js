d483e285d1813e26a3bd16175d82750ade653389
YZKA1 <estoran@163.com>
2018-11-07 15:10:24 +0800

    抽离路由模块

--- app.js
+++ app.js
@@ -4,7 +4,10 @@ const app = express();
 
 const bodyParser = require("body-parser");
 
+const moment = require('moment')
+
 const mysql = require('mysql')
+
 
 
 const conn = mysql.createConnection({
@@ -27,11 +30,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use("/node_modules", express.static("./node_modules"));
 
-// 用户请求项目首页
+// 导入router/index.js 路由模块
 
-app.get("/", (req, res) => {
-  res.render("index.ejs", { name: "zs", age: 22 });
-});
+const router1 = require('./router/index.js')
+app.use(router1)
 
 // 用户请求注册页面
 
@@ -52,16 +54,31 @@ app.post("/register", (req, res) => {
   // 判断用户输入的数据是否完整
 
   if(body.username.trim().length <= 0 || body.password.trim().length <= 0 || body.nickname.trim().length <=0) {
-      return res.send({msg:'请填写完整的表单数据在注册用户!',status:401})
+      return res.send({msg:'请填写完整的表单数据在注册用户!',status:400})
   }
   //查询用户名是否重复
 
-  const sql1 = 'select count(*) as count from blong_users where username'
+  const sql1 = 'select count(*) as count from blog_users where username= ?'
 
   conn.query(sql1,body.username,(err,result)=>{
+
     //如果查询失败,则告知客户端失败
-    if(err) return res.send({msg:'用户名查重失败!',status: 402})
-    console.log(result)
+
+    if(err) return res.send({msg:'用户名查重失败!',status: 401})
+
+    if(result[0].count !==0) return res.send({msg: '请更换其他的用户名后重新注册',status:402})
+    //执行注册的业务逻辑
+
+    body.ctime = moment().format('YYYY-MM-DD HH:mm:ss')
+
+    const sql2 = 'insert into blog_users set ?'
+
+    conn.query(sql2,body,(err,result)=>{
+
+      if(err) return res.send({msg: '注册新用户失败!',status: 404})
+
+    }) 
+    
   })
 
   res.send({ msg: "注册新用户成功!", status: 200 });
